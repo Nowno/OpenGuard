f0986053e05d747310d86420b14d2545370aa5d3
Nono <the.dark.gamer365@gmail.com>
2025-03-16 05:23:23 +0400

    forgot to commit ws_server.hpp for some reason

---
+++ core/server/ws_server.hpp
@@ -0,0 +1,32 @@
+#ifndef OPENGUARD_WS_SERVER_HPP
+#define OPENGUARD_WS_SERVER_HPP
+
+#define ASIO_STANDALONE
+#define _WEBSOCKETPP_CPP11_STL_
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include <optional>
+
+class WSServer
+{
+    public:
+    static WSServer& GetInstance();
+
+    void Poll();
+    void Send(const std::string& message);
+
+    private:
+    WSServer();
+    WSServer(const WSServer&) = delete;
+    WSServer& operator=(const WSServer&) = delete;
+
+    void OnMessage(websocketpp::connection_hdl hdl, websocketpp::server<websocketpp::config::asio>::message_ptr msg);
+    void OnOpen(websocketpp::connection_hdl hdl);
+    void OnClose(websocketpp::connection_hdl hdl);
+
+    websocketpp::server<websocketpp::config::asio> wsServer;
+    std::optional<websocketpp::connection_hdl> client;
+};
+
+#endif // OPENGUARD_WS_SERVER_HPP
